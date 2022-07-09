2a5d6801c09f0a5e582a8dfd1609ddf50ebddbca
mdhamidulislamT <hamidlive6@gmail.com>
2022-07-09 16:31:01 +0600

    some changes updated

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title', 'Add To Cart')
+
+
+@section('content')
+
+
+
+
+@endsection
+
+@section('javascripts')
+
+    <script>
+
+    </script>
+
+@endsection
