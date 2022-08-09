f5db4e1cee75b84be60d9ed3edf2a09809c774d4
mdhamidulislamT <hamidlive6@gmail.com>
2022-08-09 10:49:14 +0600

    changes

--- routes/web.php
+++ routes/web.php
@@ -135,8 +135,3 @@ Route::post('/deletePost', [CollectionController::class, 'deletePost'])->name('d
 //===  Yajra DataTables
 Route::get('/shoppingcart', [ShoppingCartController::class, 'index'])->name('shoppingcart');
 Route::get('/getCategoryWiseProducts', [ShoppingCartController::class, 'getCategoryWiseProducts'])->name('shoppingcart.getCategoryWiseProducts');
-
-
-Route::get('/greeting', function () {
-    return "Hello World. I'm from master branch";
-});
