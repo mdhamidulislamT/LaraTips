19d3863d04cbeddd3003fa18c2cfb4cfb51d7321
mdhamidulislamT <hamidlive6@gmail.com>
2022-08-18 20:01:18 +0600

    some changes updated

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -15,6 +15,7 @@ class Comment extends Model
     public function post()
     {
         return $this->belongsTo(Post::class);
+        //return $this->belongsTo(Post::class)->where('deleted', 'No');
     }
 
 }
